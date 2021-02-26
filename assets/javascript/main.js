1a64632137b07996304b68af14e736cf87ba790c
salute03 <oluwasegunogunsakin@gmail.com>
2021-02-26 20:07:09 +0000

    initial commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,7 +3,7 @@
 const panels = document.querySelectorAll('.panel');
 
  panels.forEach(panel => {
-      // () => is the same function()
+      // () => is the same as function()
     panel.addEventListener('click', function(){
         removeActiveClass()
         panel.classList.add('active')
@@ -15,3 +15,29 @@ const panels = document.querySelectorAll('.panel');
          panel.classList.remove('active')
      });
  }
+
+ const text = document.querySelector('.fill');
+ const strText = text.textContent;
+ const splitText = strText.split('');
+text.textContent = '';
+ for (let i = 0; i < splitText.length; i++) {
+     text.innerHTML += '<span>' + splitText[i] + '</span>';
+ }
+  
+/* let char = 0;
+ let timer = setInterval(onTick, 100);
+
+ function onTick() {
+     const span = text.querySelectorAll('span')[char];
+     span.classList.add('fade');
+     char++
+     if(char === splitText.length) {
+         complete();
+         return;
+     }
+ }
+
+ function complete() {
+    clearInterval(timer);
+    timer = null;
+ } */
