fa61ca4ecee8e1b5172ac2a3deccd1d4b3da7da4
salute03 <oluwasegunogunsakin@gmail.com>
2021-03-09 18:50:17 +0000

    load google map to js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -22,14 +22,8 @@ const panels = document.querySelectorAll('.panel');
 text.textContent = ''; 
  for (let i = 0; i < splitText.length; i++) {
      text.innerHTML += '<span>' + splitText[i] + '</span>';
-
-    /* const letters = text.querySelectorAll('span');
- for (let x = 0; x < letters.length; x++) {
-     span.classList.remove('fade');
- } */
  }
 
- 
  let char = 0;
  let timer = setInterval(onTick, 100);
 
@@ -41,10 +35,48 @@ text.textContent = '';
          complete();
          return;
      }
+
+     const letters = text.querySelectorAll('span');
+ for (let x = 0; x < letters.length; x++) {
+     span.classList.remove('fade');
+ } 
     
- }
+ } 
 
  function complete() {
     clearInterval(timer);
     timer = null;
    }  
+
+
+   // map js 
+
+   function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 3,
+                center: {
+                    lat: 46.619261,
+                    lng: -33.134766
+                }
+            });
+
+           var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            var locations = [
+                {lat: 40.785091, lng: -73.968285},
+                {lat: 41.084045, lng: -73.874245},
+                {lat: 40.754932, lng: -73.984016}
+            ];
+
+            var markers = locations.map(function(location, i) {
+                return new google.maps.Marker({
+                    position: location,
+                    label: labels[i % labels.length]
+                });
+            });
+
+
+            var markerCluster =  new MarkerClusterer(map, markers,
+                 {imagePath:"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",}); 
+          }
+       
