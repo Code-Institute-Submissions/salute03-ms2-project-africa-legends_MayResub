e433f6b404d28ecec60f66d62023c3d720599880
salute03 <oluwasegunogunsakin@gmail.com>
2021-05-22 17:31:36 +0000

    add interactions to google map

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,32 +5,51 @@
 
    function initMap() {
             var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
+                zoom: 5,
                 center: {
                     lat: 9.1021, 
                     lng: 18.2812
                 }
             });
-
-           var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-            var locations = [
-                {lat: 7.1475, lng: 3.3619}, 
-                {lat: 6.3716, lng: 2.0763},
-                {lat: 52.2789, lng:  5.6658}
-            ];
-
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
+             
+            //Add Marker function
+    addMarker({
+        coords:{lat: 7.1475, lng: 3.3619},
+        iconImage:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+        content:  "<h1>Fela-Anikulapo Kuti</h1>"
+    });
+    addMarker({
+        coords:{lat: 6.3716, lng: 2.0763},
+        iconImage:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+        content:  "<h1>Angélique Kidjo</h1>"
+    });
+    addMarker({
+        coords:{lat: 26.8206, lng: 30.8025},
+        iconImage:"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+        content:  "<h1>Lucky Dube</h1>"
+    });
+    
+    
+    function addMarker(props) {
+            var marker = new google.maps.Marker({
+                position:props.coords, 
+                map:map,
+                icon:props.iconImage
             });
+            
+    if (props.content) {
+        var infoWindow = new google.maps.InfoWindow({
+            content: props.content
+        });
 
+        marker.addListener("click", function(){
+            infoWindow.open(map, marker);
+        });
+    }
+          
+    }
 
-            var markerCluster =  new MarkerClusterer(map, markers,
-                 {imagePath:"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",}); 
-          }
+} 
        
 
 
